554c9b3839caa3181d09a9a2e72a120cb6d28303
Pooja Nilangekar <poojan@umd.edu>
2024-02-23 16:00:00 -0500

    Add Assignment 2

---
+++ a2/src/txn/mvcc_storage.cc
@@ -0,0 +1,81 @@
+#include "mvcc_storage.h"
+
+// Init the storage
+void MVCCStorage::InitStorage()
+{
+    for (int i = 0; i < 1000000; i++)
+    {
+        Write(i, 0, 0);
+        Mutex* key_mutex = new Mutex();
+        mutexs_[i]       = key_mutex;
+    }
+}
+
+// Free memory.
+MVCCStorage::~MVCCStorage()
+{
+    for (auto it = mvcc_data_.begin(); it != mvcc_data_.end(); ++it)
+    {
+        delete it->second;
+    }
+
+    mvcc_data_.clear();
+
+    for (auto it = mutexs_.begin(); it != mutexs_.end(); ++it)
+    {
+        delete it->second;
+    }
+
+    mutexs_.clear();
+}
+
+// Lock the key to protect its version_list. Remember to lock the key when you read/update the version_list
+void MVCCStorage::Lock(Key key)
+{
+    mutexs_[key]->Lock();
+}
+
+// Unlock the key.
+void MVCCStorage::Unlock(Key key)
+{
+    mutexs_[key]->Unlock();
+}
+
+// MVCC Read
+bool MVCCStorage::Read(Key key, Value *result, int txn_unique_id)
+{
+    //
+    // Implement this method!
+
+    // Hint: Iterate the version_lists and return the verion whose write timestamp
+    // (version_id) is the largest write timestamp less than or equal to txn_unique_id.
+    return true;
+}
+
+// Check whether the txn executed on the latest version of the key.
+bool MVCCStorage::CheckKey(Key key, int txn_unique_id)
+{
+    //
+    // Implement this method!
+
+    // Hint: Before all writes are applied (and SSI reads are validated), we need 
+    // to make sure that each key was accessed safely based on MVCC timestamp ordering
+    // protocol. This method only checks one key, so you should call this method for
+    // each key (as necessary). Return true if this key passes the check, return false if not.
+    // Note that you don't have to call Lock(key) in this method, just
+    // call Lock(key) before you call this method and call Unlock(key) afterward.
+    return true;
+}
+
+// MVCC Write, call this method only if CheckWrite return true.
+void MVCCStorage::Write(Key key, Value value, int txn_unique_id)
+{
+    //
+    // Implement this method!
+
+    // Hint: Insert a new version (malloc a Version and specify its value/version_id/max_read_id)
+    // into the version_lists. Note that InitStorage() also calls this method to init storage.
+    // Note that you don't have to call Lock(key) in this method, just
+    // call Lock(key) before you call this method and call Unlock(key) afterward.
+    // Note that the performance would be much better if you organize the versions in decreasing order.
+}
